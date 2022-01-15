6df357c1f27968bc95c5c83fe6925f1dfd9fa0b6
Sylvain Goumy <goumsss@protonmail.com>
2022-01-15 22:28:23 +0100

    update data model for matches

--- app/services/mpg.js
+++ app/services/mpg.js
@@ -8,7 +8,6 @@ const mpgToken = process.env.MPG_TOKEN;
 const mpgLeague = process.env.MPG_LEAGUE;
 const mpgSeason = process.env.MPG_SEASON;
 const mpgDivision = process.env.MPG_DIVISION;
-const mpgLeagueCode = process.env.MPG_LEAGUE;
 
 const _leagueEndpoint = (endpoint = "", league = mpgLeague) => `/league/${league}${endpoint}`;
 const _divisionEndpoint = (endpoint = "", league = mpgLeague, season = mpgSeason, division = mpgDivision) =>
@@ -40,12 +39,27 @@ const _callApi = (endpoint) => {
   });
 };
 
+let mpgTeamsData = null;
+const _getTeamsData = async () => {
+  if (!mpgTeamsData) mpgTeamsData = await _callApi(_divisionEndpoint("/teams"));
+  return mpgTeamsData;
+};
+
+let mpgClubsData = null;
+const _getClubsData = async () => {
+  if (!mpgClubsData) {
+    const { championshipClubs } = await _callApi(`/championship-clubs`);
+    mpgClubsData = championshipClubs;
+  }
+  return mpgClubsData;
+};
+
 const _createTeamRank = (team, userId, rank) => {
   return {
     userId,
     name: team.name,
     target: rank.targetMan,
-    symbol: team.abbr,
+    symbol: team.abbreviation,
     icon: team.jerseyUrl,
     points: rank.points,
     played: rank.played,
@@ -84,7 +98,6 @@ const _getSeasonRanking = async (season) => {
     if (f.previousTargetMan) targetMan = f.previousTargetMan;
     if (f.afterTargetMan) targetMan = f.afterTargetMan;
   });
-  console.log("targetMan", targetMan);
 
   const users = {};
   Object.keys(divisionData.usersTeams).forEach((k) => (users[divisionData.usersTeams[k]] = k));
@@ -149,8 +162,6 @@ module.exports.getCalendar = async (day = null) => {
   if (!day) day = divisionData.liveState.currentGameWeek;
   day = parseInt(day);
 
-  // currentMatchDay, maxMatchDay, date, live, matches
-
   const { divisionMatches } = await _callApi(_divisionEndpoint(`/game-week/${day}/matches`));
   const chId = divisionMatches[0].championshipId;
   const chSeason = divisionMatches[0].championshipSeason;
@@ -159,7 +170,7 @@ module.exports.getCalendar = async (day = null) => {
   const [chData, calendarData, teamsData] = await Promise.all([
     _callApi(`/championship-matches/${chId}/season/${chSeason}/game-week/${chWeek}`),
     _callApi(_divisionEndpoint("/calendar")),
-    _callApi(_divisionEndpoint("/teams"))
+    _getTeamsData()
   ]);
 
   const matches = divisionMatches.map((match) => {
@@ -178,44 +189,30 @@ module.exports.getCalendar = async (day = null) => {
     };
     return match;
   });
-  //console.dir(matches, { maxDepth: 10 });
 
   const results = {
     currentMatchDay: day,
     maxMatchDay: divisionData.liveState.totalGameWeeks,
     date: chData.matches[0].date,
-    live: false,
     matches
   };
-  //console.log(results);
-
   //retrieve full match details
-  /*if (data.results && (data.results.live || data.results.date < new Date().getTime())) {
-    data.results.matches = await Promise.all(
-      data.results.matches.map(async (match) => getMatch(match.id, data.results.live))
-    );
-  }*/
-
+  if (new Date(results.date) < new Date()) {
+    results.matches = await Promise.all(results.matches.map(async (match) => getMatch(match.id)));
+    if (results.matches[0].live) results.live = true;
+  }
   return results;
 };
 
-const _addPlayer = (player, defBonus, teamPlayers, teamSubstitutes) => {
+const _addPlayer = (player, teamPlayers, teamSubstitutes) => {
   if (!player.playerId && !player.id) return;
 
-  //add defBonus for starting player
-  if (player.position === 2 && defBonus && parseInt(player.number) <= 11) player.bonus = defBonus;
+  player.bonus = player.bonusRating;
 
   if (parseInt(player.number) <= 11) {
-    if (player.substitute) {
-      //replace player by its substitute for this position
-      player.substitute.number = player.number;
-      teamPlayers.push(player.substitute);
-    } else {
-      teamPlayers.push(player);
-    }
+    teamPlayers.push(player);
   } else {
     teamSubstitutes.push(player);
-    //ignore goals from substitue for live match
   }
 };
 
@@ -244,38 +241,31 @@ const addGoals = (player, teamGoals, adversaryGoals, compo) => {
   }
 };
 
-const defBonus = (composition) => {
-  if (composition > 500) return 1;
-  if (composition > 400) return 0.5;
-  return 0;
-};
-
-const calculateVirtualPlayers = (players, substitutes, defBonus) => {
+const calculateVirtualPlayers = (players, substitutes) => {
   substitutes.forEach((s) => {
-    s.subPlayer = players.find((p) => p.playerId === s.subs);
+    s.subPlayer = players.find((p) => p.playerId === s.subId);
   });
-  let availableSubstitutes = players.filter((p) => parseInt(p.number) > 11 && p.position > 1);
+  let availableSubstitutes = players.filter((p) => parseInt(p.originNumber) > 11 && p.position > 1);
 
   players.forEach((p) => {
     //set default rating and bonus if missing
     const isRotaldo = p.starter === -1 || (p.starter === 2 && !p.rating);
     if (isRotaldo) console.log(`ROTALDO => ${p.name}`);
     p.virtualRating = isRotaldo ? 2.5 : p.rating ?? 5;
-    p.virtualBonus = p.bonus ?? (!isRotaldo && p.position === 2 && defBonus && parseInt(p.number) <= 11 ? defBonus : 0);
+    p.virtualBonus = p.bonusRating;
 
     //check tactical substitute
-    const subTact = substitutes.find((s) => s.start === p.playerId);
-    //console.log(subTact);
+    const subTact = substitutes.find((s) => s.starterId === p.playerId);
     if (subTact && p.virtualRating + p.virtualBonus < subTact.rating) {
       if (subTact.subPlayer && subTact.subPlayer.rating) {
         p.virtualSubstitute = subTact.subPlayer;
         //remove substitute from available ones
-        availableSubstitutes = availableSubstitutes.filter((s) => s.playerId !== subTact.subs);
+        availableSubstitutes = availableSubstitutes.filter((s) => s.playerId !== subTact.subId);
       }
     }
 
     //check keeper auto substitute
-    if (isRotaldo && p.position === 1 && parseInt(p.number) <= 11) {
+    if (isRotaldo && p.position === 1 && parseInt(p.originNumber) <= 11) {
       const sub = players.find((s) => s.position === 1 && s.playerId !== p.playerId);
       if (sub && sub.rating) {
         console.log(`GOAL SUBSTITUE: ${sub.name} replace ${p.name}`);
@@ -285,15 +275,14 @@ const calculateVirtualPlayers = (players, substitutes, defBonus) => {
   });
 
   //check default substitute
-  console.log("original substitutes:", substitutes.map((s) => s.substituteName).join(","));
+  console.log("original substitutes:", substitutes.map((s) => s.subPlayer.name).join(","));
   console.log("available substitutes:", availableSubstitutes.map((s) => s.name).join(","));
 
   const notPlayed = players.filter(
-    (p) => (p.starter === -1 || (p.starter === 2 && !p.rating)) && parseInt(p.number) <= 11 && !p.virtualSubstitute
+    (p) =>
+      (p.starter === -1 || (p.starter === 2 && !p.rating)) && parseInt(p.originNumber) <= 11 && !p.virtualSubstitute
   );
   console.log("rotaldo??", notPlayed.map((s) => s.name).join(","));
-  if (notPlayed.length > 0) {
-  }
   notPlayed.forEach((p) => {
     let subFound = false;
     let checkingPos = p.position;
@@ -319,25 +308,25 @@ const calculateVirtualPlayers = (players, substitutes, defBonus) => {
 const calculateMpgGoals = (homePlayers, awayPlayers) => {
   //calculate line average score
   const homeScores = {
-    keeper: calculateLineScore(homePlayers.filter((p) => p.position === 1 && parseInt(p.number) <= 11)),
-    def: calculateLineScore(homePlayers.filter((p) => p.position === 2 && parseInt(p.number) <= 11)),
-    middle: calculateLineScore(homePlayers.filter((p) => p.position === 3 && parseInt(p.number) <= 11)),
-    forward: calculateLineScore(homePlayers.filter((p) => p.position === 4 && parseInt(p.number) <= 11))
+    keeper: calculateLineScore(homePlayers.filter((p) => p.position === 1 && parseInt(p.originNumber) <= 11)),
+    def: calculateLineScore(homePlayers.filter((p) => p.position === 2 && parseInt(p.originNumber) <= 11)),
+    middle: calculateLineScore(homePlayers.filter((p) => p.position === 3 && parseInt(p.originNumber) <= 11)),
+    forward: calculateLineScore(homePlayers.filter((p) => p.position === 4 && parseInt(p.originNumber) <= 11))
   };
   console.log("homeScores", homeScores);
 
   const awayScores = {
-    keeper: calculateLineScore(awayPlayers.filter((p) => p.position === 1 && parseInt(p.number) <= 11)),
-    def: calculateLineScore(awayPlayers.filter((p) => p.position === 2 && parseInt(p.number) <= 11)),
-    middle: calculateLineScore(awayPlayers.filter((p) => p.position === 3 && parseInt(p.number) <= 11)),
-    forward: calculateLineScore(awayPlayers.filter((p) => p.position === 4 && parseInt(p.number) <= 11))
+    keeper: calculateLineScore(awayPlayers.filter((p) => p.position === 1 && parseInt(p.originNumber) <= 11)),
+    def: calculateLineScore(awayPlayers.filter((p) => p.position === 2 && parseInt(p.originNumber) <= 11)),
+    middle: calculateLineScore(awayPlayers.filter((p) => p.position === 3 && parseInt(p.originNumber) <= 11)),
+    forward: calculateLineScore(awayPlayers.filter((p) => p.position === 4 && parseInt(p.originNumber) <= 11))
   };
 
   console.log("awayScores", awayScores);
 
   //calculate MPG goals...
   homePlayers
-    .filter((p) => p.position > 1 && parseInt(p.number) <= 11 && p.goals?.goal === 0)
+    .filter((p) => p.position > 1 && parseInt(p.originNumber) <= 11 && p.goals?.goal === 0)
     .forEach((p) => {
       calculatePlayerMpgGoal(p, p.virtualRating + p.virtualBonus + 0.01, awayScores);
       if (p.virtualSubstitute && p.virtualSubstitute.goals?.goal === 0) {
@@ -347,7 +336,7 @@ const calculateMpgGoals = (homePlayers, awayPlayers) => {
     });
 
   awayPlayers
-    .filter((p) => p.position > 1 && parseInt(p.number) <= 11 && p.goals?.goal === 0)
+    .filter((p) => p.position > 1 && parseInt(p.originNumber) <= 11 && p.goals?.goal === 0)
     .forEach((p) => {
       calculatePlayerMpgGoal(p, p.virtualRating + p.virtualBonus, homeScores);
       if (p.virtualSubstitute && p.virtualSubstitute.goals?.goal === 0) {
@@ -415,52 +404,134 @@ const calculateVirtualScore = (goals) => {
   return score;
 };
 
-const getMatch = async (matchId, isLive) => {
-  let data;
-  let dataLive;
-
-  if (isLive) {
-    dataLive = await _callApi(`/live/match/mpg_match_${mpgLeagueCode}_${matchId}`);
-    data = dataLive;
-  } else {
-    try {
-      dataLive = await _callApi(`/live/match/mpg_match_${mpgLeagueCode}_${matchId}`);
-      data = await _callApi(_leagueEndpoint(`/results/${matchId}`));
-    } catch (err) {
-      console.log(err);
+const _reformatPlayers = (players, clubsData, captain) => {
+  return players.map((p) => {
+    if (p.clubId) {
+      p.jerseyUrl = clubsData[p.clubId].defaultJerseyUrl;
     }
-    data = data.data;
-  }
-  data.id = matchId;
-  data.teamHome.goals = [];
-  data.teamHome.players = [];
-  data.teamHome.substitutePlayers = [];
-  data.teamHome.defBonus = defBonus(data.teamHome.composition);
-  data.teamAway.goals = [];
-  data.teamAway.players = [];
-  data.teamAway.substitutePlayers = [];
-  data.teamAway.defBonus = defBonus(data.teamAway.composition);
-  calculateVirtualPlayers(dataLive.players.home, dataLive.teamHome.substitutes, data.teamHome.defBonus);
-  calculateVirtualPlayers(dataLive.players.away, dataLive.teamAway.substitutes, data.teamAway.defBonus);
-  calculateMpgGoals(dataLive.players.home, dataLive.players.away);
-  data.players.home.forEach((player) => {
-    _addPlayer(player, data.teamHome.defBonus, data.teamHome.players, data.teamHome.substitutePlayers);
+    p.name = p.lastName;
+    p.teamid = p.clubId ? p.clubId.split("_").pop() : 0;
+    p.starter = p.status > 2 ? -1 : p.status;
+    p.goals = {
+      mpg: p.mpgGoals ?? 0,
+      goal: p.goals ?? 0,
+      own_goal: p.ownGoals ?? 0
+    };
+    if (p.playerId === captain) p.captain = true;
+    return p;
   });
-  dataLive.players.home.forEach((player) => {
-    _addPlayerGoals(player, data.teamHome.goals, data.teamAway.goals);
+};
+
+const getMatch = async (matchId) => {
+  const [
+    matchData,
+    teamsData,
+    clubsData
+    //calendarData
+  ] = await Promise.all([
+    _callApi(_divisionEndpoint(`/match/${matchId}`)),
+    _getTeamsData(),
+    _getClubsData()
+    //_callApi(_divisionEndpoint("/calendar"))
+  ]);
+
+  const teamHome = teamsData.find((t) => t.id === matchData.home.teamId);
+  matchData.teamHome = {
+    jerseyUrl: teamHome.jerseyUrl,
+    name: teamHome.name,
+    abbr: teamHome.abbreviation,
+    goals: [],
+    players: [],
+    substitutePlayers: [],
+    substitutes: matchData.home.tacticalSubs.map((sub) => {
+      sub.substituteName = matchData.home.players[sub.subId].lastName;
+      sub.starterName = matchData.home.players[sub.starterId].lastName;
+      return sub;
+    }),
+    score: matchData.home.score,
+    composition: matchData.home.composition
+  };
+  const teamAway = teamsData.find((t) => t.id === matchData.away.teamId);
+  matchData.teamAway = {
+    jerseyUrl: teamAway.jerseyUrl,
+    name: teamAway.name,
+    abbr: teamAway.abbreviation,
+    goals: [],
+    players: [],
+    substitutePlayers: [],
+    substitutes: matchData.away.tacticalSubs.map((sub) => {
+      sub.substituteName = matchData.away.players[sub.subId].lastName;
+      sub.starterName = matchData.away.players[sub.starterId].lastName;
+      return sub;
+    }),
+    score: matchData.away.score,
+    composition: matchData.away.composition
+  };
+
+  Object.keys(matchData.home.playersOnPitch).forEach((number) => {
+    const onPitch = matchData.home.playersOnPitch[number];
+    if (onPitch.playerId) {
+      if (onPitch.isSub === "mandatory" && !onPitch.starterId) {
+        // Retreive original starter player at this position
+        Object.keys(matchData.home.playersOnPitch).forEach((num) => {
+          if (num !== number && matchData.home.playersOnPitch[num].playerId === onPitch.playerId) {
+            onPitch.starterId = matchData.home.playersOnPitch[num].starterId;
+          }
+        });
+      }
+      // Calculate original position number
+      const originalPlayerId = number <= 11 ? onPitch.starterId ?? onPitch.playerId : onPitch.playerId;
+      matchData.home.players[originalPlayerId].originNumber = number;
+
+      // And add effective position number
+      if (number <= 11) matchData.home.players[onPitch.playerId].number = number;
+      else if (onPitch.starterId) matchData.home.players[onPitch.starterId].number = number;
+      else matchData.home.players[onPitch.playerId].number = number;
+    }
   });
-  data.players.away.forEach((player) => {
-    _addPlayer(player, data.teamAway.defBonus, data.teamAway.players, data.teamAway.substitutePlayers);
+  const homePlayers = _reformatPlayers(Object.values(matchData.home.players), clubsData, matchData.home.captain);
+  Object.keys(matchData.away.playersOnPitch).forEach((number) => {
+    const onPitch = matchData.away.playersOnPitch[number];
+    if (onPitch.playerId) {
+      if (onPitch.isSub === "mandatory" && !onPitch.starterId) {
+        // Retreive original starter player at this position
+        Object.keys(matchData.away.playersOnPitch).forEach((num) => {
+          if (num !== number && matchData.away.playersOnPitch[num].playerId === onPitch.playerId) {
+            onPitch.starterId = matchData.away.playersOnPitch[num].starterId;
+          }
+        });
+      }
+      // Calculate original position number
+      const originalPlayerId = number <= 11 ? onPitch.starterId ?? onPitch.playerId : onPitch.playerId;
+      matchData.away.players[originalPlayerId].originNumber = number;
+
+      // And add effective position number
+      if (number <= 11) matchData.away.players[onPitch.playerId].number = number;
+      else if (onPitch.starterId) matchData.away.players[onPitch.starterId].number = number;
+      else matchData.away.players[onPitch.playerId].number = number;
+    }
   });
-  dataLive.players.away.forEach((player) => {
-    _addPlayerGoals(player, data.teamAway.goals, data.teamHome.goals);
+  const awayPlayers = _reformatPlayers(Object.values(matchData.away.players), clubsData, matchData.away.captain);
+  calculateVirtualPlayers(homePlayers, matchData.home.tacticalSubs);
+  calculateVirtualPlayers(awayPlayers, matchData.away.tacticalSubs);
+  calculateMpgGoals(homePlayers, awayPlayers);
+  homePlayers.forEach((player) => {
+    _addPlayer(player, matchData.teamHome.players, matchData.teamHome.substitutePlayers);
+    _addPlayerGoals(player, matchData.teamHome.goals, matchData.teamAway.goals);
+  });
+  awayPlayers.forEach((player) => {
+    _addPlayer(player, matchData.teamAway.players, matchData.teamAway.substitutePlayers);
+    _addPlayerGoals(player, matchData.teamAway.goals, matchData.teamHome.goals);
   });
 
-  if (isLive && typeof data.teamHome.score === "number") {
-    data.teamHome.score = calculateVirtualScore(data.teamHome.goals);
-    data.teamAway.score = calculateVirtualScore(data.teamAway.goals);
+  if (matchData.status === 1) {
+    matchData.live = true;
+    matchData.teamHome.score = calculateVirtualScore(matchData.teamHome.goals);
+    matchData.teamAway.score = calculateVirtualScore(matchData.teamAway.goals);
   }
 
-  return data;
+  matchData.dateMatch = Object.values(matchData.championshipMatches)[0].date;
+  matchData.matchDay = matchData.divisionGameWeek;
+  return matchData;
 };
 module.exports.getMatch = getMatch;
